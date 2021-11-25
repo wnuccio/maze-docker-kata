a3f918b91f2ef1c1fae696ecbded0c845cd83b52
Walter Nuccio <walter.nuccio@it.clara.net>
2021-11-25 09:01:54 +0100

    add input.json file and Input class for AcceptanceTest

--- src/main/java/subito/kata/java/App.java
+++ src/main/java/subito/kata/java/App.java
@@ -2,5 +2,14 @@ package subito.kata.java;
 
 public class App {
     public static void main(String[] strings) {
+        Input input = readInput("input.json");
+        writeOutput(input, "output.txt");
+    }
+
+    private static Input readInput(String inputFile) {
+        return null;
+    }
+
+    private static void writeOutput(Input input, String outputFile) {
     }
 }
