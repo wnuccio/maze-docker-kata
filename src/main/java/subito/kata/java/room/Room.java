eac1f764c57dedb637fb0905920b1f7191a87bd5
Walter Nuccio <walter.nuccio@it.clara.net>
2021-11-25 14:43:28 +0100

    change toString of Room

--- src/main/java/subito/kata/java/room/Room.java
+++ src/main/java/subito/kata/java/room/Room.java
@@ -34,9 +34,7 @@ public class Room {
 
     @Override
     public String toString() {
-        return "Room{" +
-                "name='" + name + '\'' +
-                '}';
+        return String.format("Room[%s]", id);
     }
 
     public NamedObject getObject(int i) {
