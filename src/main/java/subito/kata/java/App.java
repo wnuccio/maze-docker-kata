6a83fc6f3579199aaabfe203be4c6fc8c7042e68
Walter Nuccio <walter.nuccio@it.clara.net>
2021-11-25 20:48:52 +0100

    improve code in App

--- src/main/java/subito/kata/java/App.java
+++ src/main/java/subito/kata/java/App.java
@@ -1,8 +1,8 @@
 package subito.kata.java;
 
-import subito.kata.java.inout.Input;
 import subito.kata.java.inout.InputReader;
 import subito.kata.java.inout.OutputWriter;
+import subito.kata.java.room.Maze;
 import subito.kata.java.room.Route;
 import subito.kata.java.room.RouteFinder;
 
@@ -10,12 +10,6 @@ public class App {
     private InputReader inputReader;
     private RouteFinder routeFinder;
     private OutputWriter outputWriter;
-
-    public App(InputReader inputReader, RouteFinder routeFinder, OutputWriter outputWriter) {
-        this.inputReader = inputReader;
-        this.routeFinder = routeFinder;
-        this.outputWriter = outputWriter;
-    }
 
     public static void main(String[] strings) {
         App app = new App(
@@ -26,9 +20,15 @@ public class App {
         app.execute();
     }
 
+    public App(InputReader inputReader, RouteFinder routeFinder, OutputWriter outputWriter) {
+        this.inputReader = inputReader;
+        this.routeFinder = routeFinder;
+        this.outputWriter = outputWriter;
+    }
+
     private void execute() {
-        Input input = inputReader.readInput();
-        Route route = routeFinder.findRouteFrom(input);
+        Maze maze = inputReader.readInput();
+        Route route = routeFinder.findRouteFrom(maze);
         outputWriter.writeOutput(route);
     }
 }
