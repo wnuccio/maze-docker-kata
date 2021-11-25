bc713b05548a89fecb20d05369694e3785ffe88f
Walter Nuccio <walter.nuccio@it.clara.net>
2021-11-25 08:08:21 +0100

    initial commit

---
+++ src/test/java/subito/kata/java/FirstTest.java
@@ -0,0 +1,12 @@
+package subito.kata.java;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class FirstTest {
+
+    @Test
+    void first_test() {
+        Assertions.assertThat(true).isTrue();
+    }
+}
